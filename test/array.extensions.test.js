3eb8cb917a35885b339949a695b900a8e25e3ba6
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2019-02-26 12:18:58 +0100

    test: add tests for array extensions api.

---
+++ test/array.extensions.test.js
@@ -0,0 +1,50 @@
+const BaseJoi = require('joi')
+
+const JoiArrayExtensions = require('../lib/array.extensions')
+const Joi = BaseJoi.extend(JoiArrayExtensions)
+
+describe('array extensions', () => {
+  describe('even()', () => {
+    const schema = Joi.array().even()
+
+    it('should be valid. Input is an array of even length. Expected even length.', () => {
+      const args = new Array(2).fill('w')
+
+      const result = Joi.validate(args, schema)
+
+      expect(result.error).toBeNull()
+      expect(result.value).toEqual(args)
+    })
+
+    it('should be invalid. Input is an array of odd length. Expected even length.', () => {
+      const args = new Array(3).fill('w')
+
+      const result = Joi.validate(args, schema)
+
+      expect(result.error.details[0].message).toBe('"value" must have even length')
+      expect(result.value).toEqual(args)
+    })
+  })
+
+  describe('odd()', () => {
+    const schema = Joi.array().odd()
+
+    it('should be valid. Input is an array of odd length. Expected odd length.', () => {
+      const args = new Array(3).fill('w')
+
+      const result = Joi.validate(args, schema)
+
+      expect(result.error).toBeNull()
+      expect(result.value).toEqual(args)
+    })
+
+    it('should be invalid. Input is an array of even length. Expected odd length.', () => {
+      const args = new Array(2).fill('w')
+
+      const result = Joi.validate(args, schema)
+
+      expect(result.error.details[0].message).toBe('"value" must have odd length')
+      expect(result.value).toEqual(args)
+    })
+  })
+})
